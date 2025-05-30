3123a595af50b8cefdda5ea315037ccad9cd40e5
tarsandee <supakorn.oup@dome.tu.ac.th>
2025-05-30 22:51:26 +0700

    add interest , tag
    add interest to User
    add tag to Trip

--- backend/models/Trip.js
+++ backend/models/Trip.js
@@ -6,6 +6,7 @@ const tripSchema = new mongoose.Schema({
   location: { type: String, required: true },
   date: { type: String, required: true },
   username: { type: String }, // เผื่อเก็บชื่อผู้สร้างทริป
+  tag: { type: [String], default: [] }, // แท็กสำหรับทริป
 }, { timestamps: true });
 
 export default mongoose.model("Trip", tripSchema);
